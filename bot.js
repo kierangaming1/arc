a3859d3eb3c5ab051fa7779eed04f4d770cf0a91
kierangaming1 <34714843+kierangaming1@users.noreply.github.com>
2018-09-19 18:48:44 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,13 +22,13 @@ client.on("ready", () => {
 client.on("guildCreate", guild => {
   // This event triggers when the bot joins a guild.
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+  client.user.setActivity(`Serving ArcticCAD Prefix is : +`);
 });
 
 client.on("guildDelete", guild => {
   // this event triggers when the bot is removed from a guild.
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+  client.user.setActivity(`Serving ArcticCAD Prefix is : +`);
 });
 
 
